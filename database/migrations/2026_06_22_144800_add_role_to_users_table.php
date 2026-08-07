e7a7bb2c1e3e41121f1493e0ad9d1990a81b9cf2
Nhim Dara <daracombodia54@gmail.com>
2026-08-07 23:38:39 +0700

    fix roles for SQLite seed (#7)

--- database/migrations/2026_06_22_144800_add_role_to_users_table.php
+++ database/migrations/2026_06_22_144800_add_role_to_users_table.php
@@ -9,7 +9,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->enum('role', ['user', 'admin'])->default('user')->after('email');
+            $table->enum('role', ['user', 'admin', 'super_admin'])->default('user')->after('email');
         });
     }
 
